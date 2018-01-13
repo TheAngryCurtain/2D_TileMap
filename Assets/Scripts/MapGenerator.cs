397d6b65cf55bcdb7e87a35daec7de05fbf760be
Mike <mike.d.samson@gmail.com>
2018-01-13 16:06:54 -0400

    First commit

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public enum eTileType { DeepWater, ShallowWater, Grass, Rock, Snow, Sand, Ice, PlainsGrass, JungleGrass, Tree, Mountain, Swamp };
+public enum eBiomeID { Forest, Jungle, Plains, Desert, Tundra, Swamp };
+
+[System.Serializable]
+public class Biome
+{
+    [SerializeField] private eBiomeID mBiomeID;
+    [SerializeField] private float mVegetationModifier;
+
+    [SerializeField] private Tile mPrimaryTerrainTile;
+    [SerializeField] private Tile mSecondaryTerrainTile;
+
+    [SerializeField] private Tile mPrimaryVegetationTile;
+    [SerializeField] private Tile mSecondaryVegetationTile;
+
+    public eBiomeID ID { get { return mBiomeID; } }
+    public float VegetationModifier { get { return mVegetationModifier; } }
+    public Tile PrimaryTerrainTile { get { return mPrimaryTerrainTile; } }
+    public Tile SecondaryTerrainTile { get { return mSecondaryTerrainTile; } }
+    public Tile PrimaryTreeTile { get { return mPrimaryVegetationTile; } }
+    public Tile SecondaryTreeTile { get { return mSecondaryVegetationTile; } }
+}
+
+public class MapGenerator : MonoBehaviour
+{
+    [SerializeField] private Tilemap mWaterTileMap;
+    [SerializeField] private Tilemap mTerrainTileMap;
+    [SerializeField] private Tilemap mPlantTileMap;
+
+    [SerializeField] private Biome[] mBiomes;
+
+    [SerializeField] private Tile[] mTiles;
+    [SerializeField] private int mMapSize = 10;
+
+    private void Start()
+    {
+        int seed = UnityEngine.Random.Range(0, int.MaxValue);
+        Debug.LogFormat("World Seed: {0}", seed);
+
+        BuildOcean(seed);
+        BuildTerrain(seed);
+        BuildBiomes(seed);
+    }
+
+    private void BuildOcean(int seed)
+    {
+        int halfMap = mMapSize / 2;
+        Tile currentTile = null;
+
+        PerlinGenerator oceanGen = new PerlinGenerator();
+        int oceanOctaves = 5;
+        float[][] oceanNoise = oceanGen.Generate(seed, oceanOctaves, mMapSize);
+
+        for (int y = 0; y < mMapSize; y++)
+        {
+            for (int x = 0; x < mMapSize; x++)
+            {
+                float height = oceanNoise[x][y];
+                if (height < 0.35f)
+                {
+                    currentTile = mTiles[(int)eTileType.DeepWater];
+                }
+                else
+                {
+                    currentTile = mTiles[(int)eTileType.ShallowWater];
+                }
+
+                int worldPosX = x - halfMap;
+                int worldPosY = y - halfMap;
+                Vector3Int pos = new Vector3Int(worldPosX, worldPosY, 0);
+
+                mWaterTileMap.SetTile(pos, currentTile);
+            }
+        }
+    }
+
+    private void BuildTerrain(int seed)
+    {
+        int halfMap = mMapSize / 2;
+        Tile currentTile = null;
+
+        PerlinGenerator terrainGen = new PerlinGenerator();
+        int terrainOctaves = 5;
+        float[][] terrainNoise = terrainGen.Generate(seed, terrainOctaves, mMapSize);
+
+        for (int y = 0; y < mMapSize; y++)
+        {
+            for (int x = 0; x < mMapSize; x++)
+            {
+                float height = terrainNoise[x][y];
+                if (height < 0.45f)
+                {
+                    continue; // don't cover up water
+                }
+                else if (height < 0.55f)
+                {
+                    currentTile = mTiles[(int)eTileType.Sand];
+                }
+                else if (height < 0.725f)
+                {
+                    currentTile = mTiles[(int)eTileType.Grass];
+                }
+                else if (height < 0.775f)
+                {
+                    currentTile = mTiles[(int)eTileType.Rock];
+                }
+                else if (height < 0.865f)
+                {
+                    currentTile = mTiles[(int)eTileType.Mountain];
+                }
+                else
+                {
+                    currentTile = mTiles[(int)eTileType.Snow];
+                }
+
+                int worldPosX = x - halfMap;
+                int worldPosY = y - halfMap;
+                Vector3Int pos = new Vector3Int(worldPosX, worldPosY, 0);
+
+                mTerrainTileMap.SetTile(pos, currentTile);
+            }
+        }
+    }
+
+    private void BuildBiomes(int seed)
+    {
+        int halfMap = mMapSize / 2;
+        Tile currentTerrtainTile = null;
+        System.Random rand = new System.Random(seed);
+
+        PerlinGenerator moistureGen = new PerlinGenerator();
+        int moistOctaves = 8;
+        float[][] moistureNoise = moistureGen.Generate(rand.Next(), moistOctaves, mMapSize);
+
+        PerlinGenerator temperatureGen = new PerlinGenerator();
+        int tempOctaves = 9;
+        float[][] tempNoise = temperatureGen.Generate(rand.Next(), tempOctaves, mMapSize);
+
+        PerlinGenerator vegetationGen = new PerlinGenerator();
+        int vegOctaves = 1;
+        float[][] vegNoise = vegetationGen.Generate(rand.Next(), vegOctaves, mMapSize);
+
+        for (int y = 0; y < mMapSize; y++)
+        {
+            for (int x = 0; x < mMapSize; x++)
+            {
+                int worldPosX = x - halfMap;
+                int worldPosY = y - halfMap;
+                Vector3Int pos = new Vector3Int(worldPosX, worldPosY, 0);
+                TileBase existingWaterTile = mWaterTileMap.GetTile(pos);
+                TileBase existingTerrainTile = mTerrainTileMap.GetTile(pos);
+
+                Biome currentBiome = GetBiome(moistureNoise[x][y], tempNoise[x][y]);
+
+                // don't modify certain tiles
+                if ((existingTerrainTile == null && currentBiome.ID != eBiomeID.Tundra) ||
+                    existingTerrainTile == mTiles[(int)eTileType.Rock] ||
+                    existingTerrainTile == mTiles[(int)eTileType.Mountain] ||
+                    existingTerrainTile == mTiles[(int)eTileType.Snow] )
+                {
+                    continue;
+                }
+
+                // tile from biome
+                currentTerrtainTile = currentBiome.PrimaryTerrainTile;
+                if (currentBiome.ID == eBiomeID.Tundra)
+                {
+                    if (existingTerrainTile == null)
+                    {
+                        // over water
+                        if (existingWaterTile == mTiles[(int)eTileType.ShallowWater])
+                        {
+                            currentTerrtainTile = currentBiome.SecondaryTerrainTile;
+                        }
+                        else
+                        {
+                            // deep water
+                            currentTerrtainTile = null;
+                        }
+                    }
+                }
+
+                // vegetation from biome
+                if (existingTerrainTile != null && vegNoise[x][y] > 1f - currentBiome.VegetationModifier)
+                {
+                    mPlantTileMap.SetTile(pos, currentBiome.PrimaryTreeTile);
+                }
+
+                mTerrainTileMap.SetTile(pos, currentTerrtainTile);
+            }
+        }
+    }
+
+    // TODO
+    // maybe change all of this to weight average? (see Procedural_Dungeon project Utils.cs)
+    private Biome GetBiome(float moistureValue, float tempValue)
+    {
+        if (tempValue < 0.25f)       // cold
+        {
+            return mBiomes[(int)eBiomeID.Tundra];
+        }
+        else if (tempValue < 0.45f)  // cool
+        {
+            if (moistureValue < 0.35f)
+            {
+                return mBiomes[(int)eBiomeID.Desert];
+            }
+            else if (moistureValue < 0.75f)
+            {
+                return mBiomes[(int)eBiomeID.Plains];
+            }
+            else
+            {
+                return mBiomes[(int)eBiomeID.Tundra];
+            }
+        }
+        else if (tempValue < 0.6f)  // neutral
+        {
+           if (moistureValue < 0.75f)
+            {
+                return mBiomes[(int)eBiomeID.Forest];
+            }
+            else
+            {
+                return mBiomes[(int)eBiomeID.Plains];
+            }
+        }
+        else if (tempValue < 0.75f)  // warm
+        {
+            if (moistureValue < 0.5f)
+            {
+                return mBiomes[(int)eBiomeID.Jungle];
+            }
+            else
+            {
+                return mBiomes[(int)eBiomeID.Swamp];
+            }
+        }
+        else                        // hot
+        {
+            return mBiomes[(int)eBiomeID.Desert];
+        }
+    }
+}
